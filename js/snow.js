cbaa5a20f21ab77e8987a4780f1f04f0106e60ea
heaji yoo <74044253+uheaji@users.noreply.github.com>
2022-12-19 11:31:35 +0900

    feat: 눈 내리는 효과 추가

--- js/snow.js
+++ js/snow.js
@@ -1,67 +1,17 @@
-var c = document.getElementById("canv"),
-  $ = c.getContext("2d");
-var w = (c.width = window.innerWidth),
-  h = (c.height = window.innerHeight);
+setInterval(createSnow, 300);
 
-Snowy();
-function Snowy() {
-  var snow,
-    arr = [];
-  var num = 600,
-    tsc = 1,
-    sp = 0.5; //num:눈송이 개수 , tsc:눈송이 가로 변화율, sp:내리는 속도
-  var sc = 0.8,
-    t = 10,
-    mv = 20,
-    min = 1; //sc:눈송이 크기
-  for (var i = 0; i < num; ++i) {
-    snow = new Flake();
-    snow.y = Math.random() * (h + 50);
-    snow.x = Math.random() * w;
-    snow.t = Math.random() * (Math.PI * 2);
-    snow.sz = (100 / (10 + Math.random() * 100)) * sc;
-    snow.sp = Math.pow(snow.sz * 0.8, 2) * 0.15 * sp;
-    snow.sp = snow.sp < min ? min : snow.sp;
-    arr.push(snow);
-  }
-  go();
-  function go() {
-    window.requestAnimationFrame(go);
-    $.clearRect(0, 0, w, h);
-    $.fillStyle = "hsla(242, 95%, 3%, 1)";
-    $.fillRect(0, 0, w, h);
-    $.fill();
-    for (var i = 0; i < arr.length; ++i) {
-      f = arr[i];
-      f.t += 0.05;
-      f.t = f.t >= Math.PI * 2 ? 0 : f.t;
-      f.y += f.sp;
-      f.x += Math.sin(f.t * tsc) * (f.sz * 0.3);
-      if (f.y > h + 50) f.y = -10 - Math.random() * mv;
-      if (f.x > w + mv) f.x = -mv;
-      if (f.x < -mv) f.x = w + mv;
-      f.draw();
-    }
-  }
-  function Flake() {
-    this.draw = function () {
-      this.g = $.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.sz + 8);
-      this.g.addColorStop(0, "hsla(255,255%,255%,1)");
-      this.g.addColorStop(1, "hsla(255,255%,255%,0)");
-      $.moveTo(this.x, this.y);
-      $.fillStyle = this.g;
-      $.beginPath();
-      $.arc(this.x, this.y, this.sz, 0, Math.PI * 2, true);
-      $.fill();
-    };
-  }
+function createSnow() {
+  const snow = document.createElement("i");
+  snow.classList.add("fas");
+  snow.classList.add("fa-snowflake");
+  snow.style.left = Math.random() * window.innerWidth + "px";
+  snow.style.animationDirection = Math.random() * 3 + 2 + "s";
+  snow.style.opacity = Math.random();
+  snow.style.fontSize = Math.random() * 10 + 8 + "px";
+
+  document.body.prepend(snow);
+
+  setTimeout(() => {
+    snow.remove();
+  }, 5000);
 }
-/*________________________________________*/
-window.addEventListener(
-  "resize",
-  function () {
-    c.width = w = window.innerWidth;
-    c.height = h = window.innerHeight;
-  },
-  false
-);
